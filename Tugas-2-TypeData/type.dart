d98522e179b3484dd16026d907f50c82e4175d26
fatih <fatiharahmat257@gmail.com>
2022-01-04 19:45:29 +0700

    Pengumpulan tugas 2

---
+++ Tugas-2-TypeData/type.dart
@@ -0,0 +1,51 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main(List<String> args) {
+  //NO 1
+  // var word = 'dart';
+  // var second = 'is';
+  // var third = 'awesome';
+  // var fourth = 'and';
+  // var fifth = 'I';
+  // var sixth = 'love';
+  // var seventh = 'it!';
+
+  // var result = "$word $second $third $third $fourth $fifth $sixth $seventh";
+  // print(result);
+
+  //No 2
+  // List sentence = ["I","am","going","to","be","Flutter","Developer"];
+  // var exampleFirstWord = sentence[0];
+  // var secondWord = sentence[1];
+  // var thirdWord = sentence[2];
+  // var fourthWord = sentence[3];
+  // var fifthWord = sentence[4];
+  // var sixthWord = sentence[5];
+  // var seventhWord = sentence[6];
+
+  // print('First Word: ' + exampleFirstWord);
+  // print('Second Word: ' + secondWord);
+  // print('Third Word: ' + thirdWord);
+  // print('Fourth Word: ' + fourthWord);
+  // print('Fifth Word: ' + fifthWord);
+  // print('Sixth Word: ' + sixthWord);
+  // print('Seventh Word: ' + seventhWord);
+
+  // //NO 3
+  // stdout.write("Nama Depan : ");
+  // String first_name = stdin.readLineSync()!;
+  // stdout.write("Nama Belakang : ");
+  // String last_name = stdin.readLineSync()!;
+  // stdout.write("Nama Lengkap : $first_name $last_name");
+
+  // NO 4
+  // stdout.write("Bilangan 1 : ");
+  // double x = double.parse(stdin.readLineSync()!);
+  // stdout.write("Bilangan 2 : ");
+  // double y = double.parse(stdin.readLineSync()!);
+  // print("Penjumlahan : ${x + y}");
+  // print("Pengurangan : ${x - y}");
+  // print("Pembagian : ${x / y}");
+  // print("Perkalian : ${x * y}");
+}
