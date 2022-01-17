0e535dc8f3a3069e48cfb2d333bf04c536aef6da
fatlhr <fatiharahmat257@gmail.com>
2022-01-17 16:50:39 +0700

    Pengumpulan Tugas 11

---
+++ sanberappflutter/lib/tugas/tugas11/Telegram.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'Chart_model.dart';
+import 'DrawerScreen.dart';
+
+class Telegram extends StatefulWidget {
+  Telegram({Key? key}) : super(key: key);
+
+  @override
+  _TelegramState createState() => _TelegramState();
+}
+
+class _TelegramState extends State<Telegram> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Telegram"),
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      drawer: DrawerScreen(),
+      body: ListView.separated(
+          itemBuilder: (ctx, i) {
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 28,
+                backgroundImage: NetworkImage(items[i].profileUrl!),
+              ),
+              title: Text(
+                items[i].name!,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text(items[i].message!),
+              trailing: Text(items[i].time!),
+            );
+          },
+          separatorBuilder: (ctx, i) {
+            return Divider();
+          },
+          itemCount: items.length),
+    );
+  }
+}
